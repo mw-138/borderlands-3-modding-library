68cf0028dac6a3f217a796a5af8540b972915405
mw138 <mattheww138@gmail.com>
2024-03-06 21:35:18 +0000

    Update POIDataPaths.cs

--- DataPaths/POIDataPaths.cs
+++ DataPaths/POIDataPaths.cs
@@ -3,4 +3,5 @@
 public static class POIDataPaths
 {
     public static readonly string LootRoom = "/Game/PatchDLC/Ixora/GameData/ZoneMap/POI/POI_LootRoom.POI_LootRoom";
+    public static readonly string LootLegendary = "/Game/GameData/ZoneMap/POI_Types/POI_LegendaryPickup.POI_LegendaryPickup";
 }
