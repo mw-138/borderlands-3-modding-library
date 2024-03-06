7671c7ddef85f3ec050e2cd8ec902fa1de03d015
mw138 <mattheww138@gmail.com>
2024-03-06 21:06:55 +0000

    Create SetItemNameHotfix.cs

---
+++ Hotfixes/Custom/SetItemNameHotfix.cs
@@ -0,0 +1,23 @@
+﻿using Borderlands3ModdingLibrary.Patches;
+
+namespace Borderlands3ModdingLibrary.Hotfixes.Custom;
+
+public class SetItemNameHotfix(string itemPath, string name, string comment = "", List<Patch> patches = default) : Hotfix(comment, patches)
+{
+    public override List<Patch> GetPatches()
+    {
+        return
+        [
+            new Patch(
+                PatchOperation.SparkPatchEntry,
+                PatchType.Regular,
+                $"{itemPath},PartName,string,,{name}"
+            )
+        ];
+    }
+
+    public override string GetComment()
+    {
+        return $"Renames the item at path '{itemPath}' to '{name}'";
+    }
+}
