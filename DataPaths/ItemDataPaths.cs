36d7cad256834c7e0d08f028f52b2ffe54607c1c
mw138 <mattheww138@gmail.com>
2024-03-06 21:04:38 +0000

    Create ItemDataPaths.cs

---
+++ DataPaths/ItemDataPaths.cs
@@ -0,0 +1,12 @@
+﻿namespace Borderlands3ModdingLibrary.DataPaths;
+
+public static class ItemDataPaths
+{
+    public static class UniqueWeapons
+    {
+        public static readonly string TheKillingWordRoot = "/Game/Gear/Weapons/Pistols/ChildrenOfTheVault/_Shared/_Design/_Unique/Mouthpiece";
+        public static readonly string TheKillingWord = $"{TheKillingWordRoot}/Balance/Balance_PS_COV_Mouthpiece.Balance_PS_COV_Mouthpiece";
+        public static readonly string TheKillingWordRedText = $"{TheKillingWordRoot}/UIStat_RedText_Mouthpiece.UIStat_RedText_Mouthpiece";
+        public static readonly string TheKillingWordName = $"{TheKillingWordRoot}/Name_COV_PS_Mouthpiece.Name_COV_PS_Mouthpiece";
+    }
+}
