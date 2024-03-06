c48746385ed3d9dd36e68cedb9d548ce90316e46
mw138 <mattheww138@gmail.com>
2024-03-06 21:04:35 +0000

    Create AudioDataPaths.cs

---
+++ DataPaths/AudioDataPaths.cs
@@ -0,0 +1,6 @@
+﻿namespace Borderlands3ModdingLibrary.DataPaths;
+
+public static class AudioDataPaths
+{
+    public static readonly string LootSpawnLegendary = "/Game/Audio/Events/UX/HUD/Loot/WE_Loot_Spawn_Legendary.WE_Loot_Spawn_Legendary";
+}
