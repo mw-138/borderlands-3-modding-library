e9d2010ab07db0244a637a3d0d389df81f30a3ba
mw138 <mattheww138@gmail.com>
2024-03-06 21:07:13 +0000

    Update SetRarityScoreMultiplierHotfix.cs

--- Hotfixes/Custom/SetRarityScoreMultiplierHotfix.cs
+++ Hotfixes/Custom/SetRarityScoreMultiplierHotfix.cs
@@ -1,9 +1,10 @@
-﻿using Borderlands3ModdingLibrary.Patches;
+﻿using Borderlands3ModdingLibrary.DataPaths;
+using Borderlands3ModdingLibrary.Patches;
 using Borderlands3ModdingLibrary.Rarities;
 
 namespace Borderlands3ModdingLibrary.Hotfixes.Custom;
 
-public class SetRarityScoreMultiplierHotfix(Rarity rarity, float scoreMultiplier, string comment = "", List<Patch> patches = null) : Hotfix(comment, patches)
+public class SetRarityScoreMultiplierHotfix(Rarity rarity, float scoreMultiplier, string comment = "", List<Patch> patches = default) : Hotfix(comment, patches)
 {
     public override List<Patch> GetPatches()
     {
@@ -15,12 +16,12 @@ public class SetRarityScoreMultiplierHotfix(Rarity rarity, float scoreMultiplier
                 $"{RarityDataPaths.GetPath(rarity)},ItemScoreRarityModifier.BaseValueConstant,0,,{scoreMultiplier}",
                 comment: $"Sets the score multiplier for {rarity} to {scoreMultiplier}"
             ),
-            new Patch(
-                PatchOperation.SparkPatchEntry,
-                PatchType.Regular,
-                $"/Game/PatchDLC/Hibiscus/Streaming/Data/RarityData_MoxxiDrink.RarityData_MoxxiDrink,ItemScoreRarityModifier.BaseValueConstant,0,,{scoreMultiplier}",
-                comment: $"Sets the score multiplier for {rarity} to {scoreMultiplier} - (Alternative)"
-            ),
+            //new Patch(
+            //    PatchOperation.SparkPatchEntry,
+            //    PatchType.Regular,
+            //    $"/Game/PatchDLC/Hibiscus/Streaming/Data/RarityData_MoxxiDrink.RarityData_MoxxiDrink,ItemScoreRarityModifier.BaseValueConstant,0,,{scoreMultiplier}",
+            //    comment: $"Sets the score multiplier for {rarity} to {scoreMultiplier} - (Alternative)"
+            //),
         ];
     }
 
