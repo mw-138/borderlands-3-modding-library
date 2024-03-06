035c42a47240df619ece38344edfa132c2b6383a
mw138 <mattheww138@gmail.com>
2024-03-06 21:04:45 +0000

    Create POIDataPaths.cs

---
+++ DataPaths/POIDataPaths.cs
@@ -0,0 +1,6 @@
+﻿namespace Borderlands3ModdingLibrary.DataPaths;
+
+public static class POIDataPaths
+{
+    public static readonly string LootRoom = "/Game/PatchDLC/Ixora/GameData/ZoneMap/POI/POI_LootRoom.POI_LootRoom";
+}
