a731ac7581d8a303253b0e046b60212c758ee722
mw138 <mattheww138@gmail.com>
2024-03-06 21:07:30 +0000

    Update SetRarityBeamColorPatch.cs

--- Patches/Custom/SetRarityBeamColorPatch.cs
+++ Patches/Custom/SetRarityBeamColorPatch.cs
@@ -1,4 +1,5 @@
 ﻿using Borderlands3ModdingLibrary.Classes;
+using Borderlands3ModdingLibrary.DataPaths;
 using Borderlands3ModdingLibrary.Rarities;
 
 namespace Borderlands3ModdingLibrary.Patches.Custom;
