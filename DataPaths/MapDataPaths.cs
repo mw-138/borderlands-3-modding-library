e9cfe2b81424ccd05022d91ae3da20c5a9875f96
Matthew <49130090+mw-138@users.noreply.github.com>
2024-09-26 20:42:48 +0100

    Update MapDataPaths.cs

--- DataPaths/MapDataPaths.cs
+++ DataPaths/MapDataPaths.cs
@@ -3,4 +3,5 @@
 public static class MapDataPaths
 {
     public static readonly string Sanctuary = "/Game/Maps/Sanctuary3";
+    public static readonly string MotorcadeFestival = "/Game/Maps/Zone_3/MotorcadeFestival";
 }
